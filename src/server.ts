3392b4017c993326248fdc0148f4eeccaa0f4c00
Matthew Arellano Roxas <154869976+matthew-arellano-roxas@users.noreply.github.com>
2026-05-20 00:33:57 +0800

    Initial commit

---
+++ src/server.ts
@@ -0,0 +1,10 @@
+import '@/helpers/loadEnv';
+
+import express from 'express';
+import { env } from '@/config';
+
+const app = express();
+
+app.listen(env.PORT, () => {
+  console.log(`Server running at http://localhost:${env.PORT}`);
+});
